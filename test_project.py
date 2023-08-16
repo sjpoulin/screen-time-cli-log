61337023c3c185ff12a3b93b41aefe30cd6a82d8
Spencer Poulin <spencerpoulin@Lauras-MacBook-Air-2.local>
2023-08-16 12:18:11 -0400

    Updated pytest

--- test_project.py
+++ test_project.py
@@ -1,4 +1,4 @@
 from project import fetch_data, graph_data, menu
 
 def test_menu():
-    assert menu() == "Welcome to Screentime!\n\nType 'log' to log an entry\nType 'graph' to view a graph of your screen time\nType 'exit' to exit"
+    assert menu() == "Welcome to Screentime!\n\nType 'log' to log an entry\nType 'graph' to view a graph of your screen time\nType 'delete' to reset your data\nType 'exit' to exit"
