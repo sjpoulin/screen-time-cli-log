f12203691fd92b5fc26579745a04993220688adc
Spencer Poulin <spencerpoulin@Lauras-MacBook-Air-2.local>
2023-09-05 11:28:23 -0400

    Delete branch and revert date input

--- test_project.py
+++ test_project.py
@@ -1,4 +1,5 @@
 from project import fetch_data, graph_data, menu
 
+
 def test_menu():
     assert menu() == "Welcome to Screentime!\n\nType 'log' to log an entry\nType 'graph' to view a graph of your screen time\nType 'delete' to reset your data\nType 'exit' to exit"
